fec25d2daaf41bd8d44e6ae91971c8554f2ddd5b
Alberto Conde moreno <aconde-m@c2r9s1.42malaga.com>
2022-04-21 14:24:53 +0200

    trabajando en LIBFT

---
+++ Lib_Ft/libft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aconde-m <aconde-m@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/21 10:28:32 by aconde-m          #+#    #+#             */
+/*   Updated: 2022/04/21 12:24:53 by aconde-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef  LIBFT_H
+# define LIBFT_H
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+int ft_isalpha(int c);
+int ft_isdigit(int c);
+#endif
